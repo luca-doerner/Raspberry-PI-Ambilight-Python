5d242b108716fb901492927b2d26a39c0ce8f099
luca <doerner.luca@googlemail.com>
2025-02-09 20:55:51 +0100

    fertig (100%)

--- src/fertig.py
+++ src/fertig.py
@@ -1,18 +1,18 @@
 import multiprocessing as mp
+import board
+import neopixel
 import time
 import mss
 import numpy as np
 import cv2
-from rpi_ws281x import PixelStrip, Color  # Falls du WS2812 LEDs nutzt
 
 # LED-Konfiguration (Anpassen an dein Setup)
 LED_COUNT = 220
-LED_PIN = 18
-LED_BRIGHTNESS = 150
+PIN = board.D18
+LED_BRIGHTNESS = 0.7
 
-# LED-Strip initialisieren
-strip = PixelStrip(LED_COUNT, LED_PIN, brightness=LED_BRIGHTNESS)
-strip.begin()
+# Initialize NeoPixel object
+pixels = neopixel.NeoPixel(PIN, LED_COUNT, brightness=0.7, auto_write=False)
 
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 
@@ -20,31 +20,38 @@ if not cap.isOpened():
     print("Fehler: HDMI-Capture-Device nicht gefunden!")
     exit()
 
-def get_dominant_color():
+def get_dominant_color(q):
     ret, frame = cap.read()
     if not ret:
         print("Kein HDMI-Signal!")
         return
 
-    resized = cv2.resize(frame, (1, 1), interpolation=cv2.INTER_LINEAR)
-    colors = resized[0, 0].toList()
-    q_out.put(colors)
+    while True:
+        ret, frame = cap.read()
+        resized = cv2.resize(frame, (70, 40), interpolation=cv2.INTER_LINEAR)
+        q.put(resized)
 
 def update_leds(q):
     """ LEDs aktualisieren """
     while True:
         colors = q.get()
         for i in range(LED_COUNT):
-            strip.setPixelColor(i, Color(int(colors[2]), int(colors[1]), int(colors[0])))  # BGR zu RGB
-        strip.show()
+            if(i >= 150):
+                pixels[i] = colors[i - 150-1, 40-1]
+            elif(i >= 110):
+                pixels[i] = colors[70-1, i - 110-1]
+            elif(i >= 40):
+                pixels[i] = colors[i - 40-1, 1-1]
+            else:
+                pixels[i] = colors[1-1, i]
+        pixels.show()
         print("LEDs Updated")
 
 if __name__ == "__main__":
-    q_screen = mp.Queue()
-    q_colors = mp.Queue()
+    q = mp.Queue()
 
-    p1 = mp.Process(target=get_dominant_color(), args=(q_screen,))
-    p2 = mp.Process(target=update_leds, args=(q_colors,))
+    p1 = mp.Process(target=get_dominant_color, args=(q,))
+    p2 = mp.Process(target=update_leds, args=(q,))
 
     p1.start()
     p2.start()
