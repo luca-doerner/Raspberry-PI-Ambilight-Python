8b3cc60668b3beb32ce3244268a27a80e52691b5
Luca Doerner <81716057+luca-doerner@users.noreply.github.com>
2025-02-22 08:11:14 +0000

    zackomatiko

--- src/aus.py
+++ src/aus.py
@@ -1,9 +1,6 @@
 import board
 import neopixel
 
-# Initialize NeoPixel object
-pixels = neopixel.NeoPixel(PIN, LED_COUNT, brightness=1, auto_write=False)
-
 # LED configuration
 with open("config.json", "r") as file:
     data = json.load(file)
@@ -14,6 +11,9 @@ LED_COUNT_RIGHT = data["count_right"]
 LED_COUNT_BOTTOM = data["count_bottom"]
 LED_COUNT = LED_COUNT_BOTTOM + LED_COUNT_RIGHT + LED_COUNT_TOP + LED_COUNT_LEFT
 PIN = board.D18
+
+# Initialize NeoPixel object
+pixels = neopixel.NeoPixel(PIN, LED_COUNT, brightness=1, auto_write=False)
 
 pixels.fill((0,0,0))
 pixels.show()
